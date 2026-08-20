b70ddec5c664dea006b6956168972c8137b2c6b0
Hemalatha Lingeswaran <hemalatha.lingeswaran@bi3technologies.com>
2026-08-20 16:21:03 +0530

    set branch-b content

--- module1.py
+++ module1.py
@@ -12,37 +12,7 @@ print(df1.head(5))
 
 print(df1.isnull().sum())
 
-
-# checking the value is valid or not
-df1['amount_status'] = df1['amount'].apply(lambda x: 'Valid' if x > 0 else 'Flagged')
-
-flagged_rows = df1[df1['amount'] <= 0]
-
-if flagged_rows.empty:
-    print("All transaction amounts are positive! No issues found.")
-else:
-    print(flagged_rows[['transaction_id', 'customer_id', 'amount']])
-
-
-# Aggregate transactions by date (date, total_amount, transaction_count). Sort by date.
-df1['date'] = pd.to_datetime(df1['date'])
-
-summary = df1.groupby('date').agg(
-    total_amount=('amount', 'sum'),
-    transaction_count=('transaction_id', 'count')
-).reset_index()
-
-# Sorting the results 
-summary = summary.sort_values(by='date')
-
-print("exit")
-print(summary)
-
-print("the summary was successfully generated")
-
-summary.to_csv("daily_sales_summary.csv", index=False)
-
-
+print("successfully completed")
 
 
 
